85baf3a1d927100cf84660827776052963d4b88a
JuanCarlosLeonel <juancble94@hotmail.com>
2022-07-05 21:24:02 -0300

    ativacaoUsuario_por_TOKEN

--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -137,3 +137,8 @@ MESSAGE_TAGS = {
     constants.INFO: 'alert-info',
     constants.WARNING: 'alert-warning',
 }
+
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = "juancble94@hotmail.com"
