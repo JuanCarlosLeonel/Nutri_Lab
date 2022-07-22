7b291ad41812a2c323c64fc80d8ad17baf9cfbd1
JuanCarlosLeonel <juancble94@hotmail.com>
2022-07-22 20:42:20 -0300

    dadospaciente_pornutricionistaLogado Gráfico_Dadoslaboratoriais

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . views import pacientes
+from . views import pacientes, dados_paciente_listar, dados_paciente, grafico_peso
 
 urlpatterns = [
     path('pacientes/', pacientes, name='pacientes'),
+    path('dados_paciente/', dados_paciente_listar, name="dados_paciente_listar"),
+    path('dados_paciente/<str:id>/', dados_paciente, name="dados_paciente"),
+    path('grafico_peso/<str:id>/', grafico_peso, name="grafico_peso")
 
 ]
